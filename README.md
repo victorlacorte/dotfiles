4b226361aa4ad01699529c3f0f2435e125f96895
victorlacorte <victorlacorte@live.com>
2022-03-07 18:41:08 -0300

    feat: macOS related notes/configs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dotfiles
 
-## TODO
+## TODO MacBook
 
 - [Trackpad gestures](https://support.apple.com/en-us/HT204895)
 
@@ -9,16 +9,14 @@
 
 - Repeating characters: `defaults write -g ApplePressAndHoldEnabled -bool false`
 
-- Switch keyboard layouts: ctrl + `<space bar>`
-
-- Sometimes I get the error `"sign_and_send_pubkey: signing failed"` from the
-  gpg-agent. The fix seems to be running `gpg-connect-agent updatestartuptty
-/bye`
-
-- adopt ~/.editorconfig
+- Switch keyboard layouts: ctrl + `<Tab>`
 
 - [Copy and paste from the terminal](https://apple.stackexchange.com/a/15322):
   `pbcopy and pbpaste` 
 
 - Clocker app for improved timezone viewing
+
+- GnuPG issues:
+    - [Brew formula](https://formulae.brew.sh/formula/pinentry-mac)
+    - [SO discussion](https://stackoverflow.com/a/40066889/1643883)
 
--- setup.sh
+++ setup.sh
@@ -6,142 +6,172 @@ set -euo pipefail
 # 	command -v "$@" > /dev/null 2>&1
 # }
 
-# Works for bash and zsh
-function download_git_completion() {
-  curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.local/git/git-completion.bash
+
+download_nvim_plug() {
+    curl -fsSL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o ~/.config/nvim/autoload/plug.vim --create-dirs
 }
 
-function install_macos_homebrew() {
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#function create_vim_undodir() {
+#  mkdir -p "$HOME/.vim/undodir"
+#}
+
+
+# Works for bash and zsh
+download_git_completion() {
+    curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.local/git/git-completion.bash
 }
+
+
+install_macos_homebrew() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
 
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-function install_macos_aws_cli() {
-  curl -fsSL "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+install_macos_aws_cli() {
+    curl -fsSL "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 }
 
-function install_macos_rust() {
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+install_macos_rust() {
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
-function create_dirs() {
-  mkdir -p "$HOME/Downloads"
-  mkdir -p "$HOME/bin"
+
+create_dirs() {
+    mkdir -p "$HOME/Downloads"
+    mkdir -p "$HOME/bin"
 }
 
-function apt_update() {
-  sudo apt-get update -qq > /dev/null
+
+apt_update() {
+    sudo apt-get update -qq > /dev/null
 }
 
-function apt_install() {
-  sudo apt-get install -qq --no-install-recommends "$@" > /dev/null
+
+apt_install() {
+    sudo apt-get install -qq --no-install-recommends "$@" > /dev/null
 }
+
 
 # Notice we assume a Debian x64 installation
-function install_docker() {
-  local apt_repo="deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-  local pre_reqs='docker-ce docker-ce-cli containerd.io'
+install_docker() {
+    local apt_repo="deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    local pre_reqs='docker-ce docker-ce-cli containerd.io'
 
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
-  
-  echo "$apt_repo" > /etc/apt/sources.list.d/docker.list
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-  apt_update
-  apt_install $pre_reqs
+    echo "$apt_repo" > /etc/apt/sources.list.d/docker.list
+
+    apt_update
+    apt_install $pre_reqs
 }
 
-function post_install_docker() {
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
+
+post_install_docker() {
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
 }
 
-# https://dev.to/bowmanjd/install-docker-on-windows-wsl-without-docker-desktop-34m9 
-function setup_docker_wsl2() {
-  # Share a common group ID for group docker
-  local common_group_id=36257
-  local docker_dir=/mnt/wsl/shared-docker
-  local sudoers_file=/etc/sudoers.d/99_docker
 
-  if getent group | grep -q $common_group_id; then
-    echo "Group ID $common_group_id not available. Choose a different number"
-    exit 1
-  fi
+# https://dev.to/bowmanjd/install-docker-on-windows-wsl-without-docker-desktop-34m9
+setup_docker_wsl2() {
+    # Share a common group ID for group docker
+    local common_group_id=36257
+    local docker_dir=/mnt/wsl/shared-docker
+    local sudoers_file=/etc/sudoers.d/99_docker
 
-  sudo groupmod -g $common_group_id docker
+    if getent group | grep -q $common_group_id; then
+        echo "Group ID $common_group_id not available. Choose a different number"
+        exit 1
+    fi
 
-  sudo mkdir /etc/docker
-  printf "{\n\t\"hosts\": [\"unix://$docker_dir/docker.sock\"],\n\t\"iptables\": false\n}" > /etc/docker/daemon.json
+    sudo groupmod -g $common_group_id docker
 
-  sudo bash -c "echo '%docker ALL=(ALL)  NOPASSWD: /usr/bin/dockerd' >> $sudoers_file"
-  sudo chmod 440 $sudoers_file
+    sudo mkdir /etc/docker
+    printf "{\n\t\"hosts\": [\"unix://$docker_dir/docker.sock\"],\n\t\"iptables\": false\n}" > /etc/docker/daemon.json
 
-  # intentionally leave out the -q flag to display error messages
-  if ! sudo visudo -cf $sudoers_file > /dev/null; then
-    exit 1
-  fi
+    sudo bash -c "echo '%docker ALL=(ALL)  NOPASSWD: /usr/bin/dockerd' >> $sudoers_file"
+    sudo chmod 440 $sudoers_file
+
+    # intentionally leave out the -q flag to display error messages
+    if ! sudo visudo -cf $sudoers_file > /dev/null; then
+        exit 1
+    fi
 }
+
 
 # https://ohmyposh.dev/docs/linux
 # does not work for macOS! https://ohmyposh.dev/docs/macos
-function install_oh_my_posh() {
-  local exec="$HOME/bin/oh-my-posh"
-  # local themes="$HOME/.local/oh-my-posh/themes"
+install_oh_my_posh() {
+    local bin_file="$HOME/bin/oh-my-posh"
+    # local themes="$HOME/.config/oh-my-posh/themes"
 
-  # mkdir -p "$themes"
+    # mkdir -p "$themes"
 
-  curl -fsSL https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -o "$exec"
-  sudo chmod +x "$exec"
+    curl -fsSL https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -o "$bin_file"
+    sudo chmod +x "$bin_file"
 
-  # curl -fsSL https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o "$themes/themes.zip"
-  # unzip "$themes/themes.zip"
-  # rm "$themes/themes.zip"
-  # chmod u+rw "$themes/*.json"
+    # curl -fsSL https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o "$themes/themes.zip"
+    # unzip "$themes/themes.zip"
+    # rm "$themes/themes.zip"
+    # chmod u+rw "$themes/*.json"
 }
 
-function install_volta() {
-  curl -fsSL https://get.volta.sh | bash -s -- --skip-setup > /dev/null
+
+install_volta() {
+    curl -fsSL https://get.volta.sh | bash -s -- --skip-setup > /dev/null
 }
 
-function download_caskaydia_cove_font() {
-  download_nerd_font CascadiaCode Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete%20Mono.otf
+
+download_caskaydia_cove_font() {
+    download_nerd_font CascadiaCode Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete%20Mono.otf
 }
 
-function download_hack_font() {
-  download_nerd_font Hack Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+
+download_hack_font() {
+    download_nerd_font Hack Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 }
 
-function download_nerd_font() {
-  local patched_font=$1
-  local font_file=$2
 
-  # echo "Downloading $patched_font font..." 
-  local url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/$patched_font/Regular/complete/$font_file"
+download_nerd_font() {
+    local patched_font=$1
+    local font_file=$2
 
-  curl -fsSL "$url" -o "$HOME/Downloads/$(echo $font_file | tr -d %20)"
+    # echo "Downloading $patched_font font..."
+    local url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/$patched_font/Regular/complete/$font_file"
+
+    curl -fsSL "$url" -o "$HOME/Downloads/$(echo $font_file | tr -d %20)"
 }
 
-function stow_debian() {
-  stow --dotfiles --target $HOME common debian
+
+stow_debian() {
+    stow --dotfiles --target $HOME common debian
 }
 
-function stow_wsl2_debian() {
-  stow --dotfiles --target $HOME common wsl2_debian
+
+stow_wsl2_debian() {
+    stow --dotfiles --target $HOME common wsl2_debian
 }
+
 
 # Assumes the .vimrc has already been stowed
-function setup_vundle() {
-  git clone -q https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
+# @deprecated Utilize vim-plug instead
+setup_vundle() {
+    git clone -q https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 
-  # https://vi.stackexchange.com/a/15564
-  # Setup without prompting for confirmation or displaying misleading error
-  # messages
-  vim -E -s -u "$HOME/.vimrc" +PluginInstall +qall
+    # https://vi.stackexchange.com/a/15564
+    # Setup without prompting for confirmation or displaying misleading error
+    # messages
+    vim -E -s -u "$HOME/.vimrc" +PluginInstall +qall
 }
+
 
 # APT specific
 # Docker is installed on another function
-function install_requirements() {
-  local pre_reqs='
+install_requirements() {
+    local pre_reqs='
     apt-transport-https
     bash-completion
     ca-certificates
@@ -156,24 +186,31 @@ function install_requirements() {
     stow
     tmux
     vim-gtk
-  '
+    '
 
-  apt_update
-  apt_install $pre_reqs
+    apt_update
+    apt_install $pre_reqs
 }
 
-#function brew_install_requirements() {
-#  # https://ohmyposh.dev/docs/macos
-#  #gnupg aka gnupg2
-#}
 
-# TODO GnuPG + SSH authentication
-#install_requirements
-#setup_docker_wsl2
-#setup_vundle
-#install_oh_my_posh
-#download_caskaydia_cove_font
-#install_volta
-#install_macos_homebrew
-#download_git_completion
-download_hack_font
+# `ripgrep` and `fd` were installed after performing `:checkhealth telescope`
+# https://github.com/nvim-telescope/telescope.nvim
+# https://github.com/burntsushi/ripgrep
+# https://github.com/sharkdp/fd
+brew_install_requirements() {
+    # https://ohmyposh.dev/docs/macos
+    #gnupg aka gnupg2
+
+    # TODO finish the extensive list
+    local pre_reqs='
+    fd
+    iterm2
+    neovim
+    ripgrep
+    stow
+    tmux
+    '
+
+    brew install $pre_reqs
+}
+
